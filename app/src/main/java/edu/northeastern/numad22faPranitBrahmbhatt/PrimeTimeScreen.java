0a776abb25d9d5142083237e5335135893c8b7ff
Pranit Brahmbhatt <pranit@esamudaay.com>
2022-10-07 04:40:06 -0400

    prime numbers getting displayed correctly

--- app/src/main/java/edu/northeastern/numad22faPranitBrahmbhatt/PrimeTimeScreen.java
+++ app/src/main/java/edu/northeastern/numad22faPranitBrahmbhatt/PrimeTimeScreen.java
@@ -1,16 +1,23 @@
 package edu.northeastern.numad22faPranitBrahmbhatt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PrimeTimeScreen extends AppCompatActivity {
 
     int i, j, prime = 3, lastNum, count = 0;
     boolean endThread = false, check = true;
+    TextView onClickChangeLastPrimeText;
+    TextView onClickChangeLastNumberText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +27,19 @@ public class PrimeTimeScreen extends AppCompatActivity {
         Button findPrimesButton = findViewById(R.id.find_prime_button);
         Button terminateSearchButton = findViewById(R.id.terminate_search_button);
 
+        onClickChangeLastPrimeText = findViewById(R.id.PrimeNumberDisplayed);
+        onClickChangeLastNumberText = findViewById(R.id.LastNumberDisplayed);
+
         Thread findPrimeThread = new Thread(
                 new Runnable() {
 
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void run() {
                         while (!endThread) {
-                            for(i = 3; i <= 20; i = i + 2) {
-                                for(j = 2; j <= i / 2; j++){
-                                    if(i % j == 0){
+                            for (i = 3; i <= 10000; i = i + 2) {
+                                for (j = 2; j <= i / 2; j++) {
+                                    if (i % j == 0) {
                                         check = false;
                                         break;
                                     }
@@ -56,14 +67,48 @@ public class PrimeTimeScreen extends AppCompatActivity {
 
         terminateSearchButton.setOnClickListener(
                 new View.OnClickListener() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void onClick(View view) {
                         endThread = true;
                         System.out.println("Thread Terminated");
                         System.out.println(prime);
                         System.out.println(lastNum);
+                        onClickChangeLastPrimeText.setText(
+                                Integer.toString(prime)
+                        );
+                        onClickChangeLastNumberText.setText(
+                                Integer.toString(lastNum)
+                        );
                     }
                 }
         );
+
+//        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+//            @Override
+//            public void handleOnBackPressed() {
+//                AlertDialog.Builder alertdialog = new AlertDialog.Builder(this);
+//                alertdialog.setTitle("Alert!");
+//                alertdialog.setMessage("Are you sure you want to terminate the Prime search?");
+//                alertdialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        finish();
+//                    }
+//                });
+//
+//                alertdialog.setNegativeButton("No", new DialogInterface.OnClickListener(){
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.cancel();
+//                    }
+//                });
+//
+//                AlertDialog alert=alertdialog.create();
+//                alertdialog.show();
+//
+//            }
+//        };
+
     }
 }
